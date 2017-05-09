27018acaab737fc3651680e86a281acd825f194a
trietna <nguyenanhtriet@gmail.com>
2017-05-09 15:49:50 +0700

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data<-read.table("household_power_consumption.txt",sep=";",dec=".",header=T,stringsAsFactors =F )
+datasub<-subset(data,data$Date=="1/2/2007"|data$Date=="2/2/2007")
+gap<-as.numeric(datasub$Global_active_power)
+png("plot1.png",width=480,height = 480)
+hist(x=gap,col = "red",main = "Global Active Power",xlab = "Global Active Power (kilowatts)")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,7 @@
+data<-read.table("household_power_consumption.txt",sep=";",dec=".",header=T,stringsAsFactors =F)
+datasub<-subset(data,data$Date=="1/2/2007"|data$Date=="2/2/2007")
+gap<-as.numeric(datasub$Global_active_power)
+datatime<-strptime(paste(datasub$Date,datasub$Time),"%d/%m/%Y %H:%M:%S")
+png("plot2.png",width=480,height = 480)
+plot(datatime,gap,type = "l",xlab="",ylab="Global Active Power (kilowatts)")
+dev.off()
